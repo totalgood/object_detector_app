63dc5be6a317d5d37438e9baeb07371b40c590e8
Hobson Lane <github@totalgood.com>
2017-11-28 04:36:04 +0100

    adjust threshold back up to .5 (two places)

--- object_detection/utils/nlp.py
+++ object_detection/utils/nlp.py
@@ -123,7 +123,7 @@ def pluralize(s):
         return word + 's'
 
 
-def update_state(boxes, classes, scores, category_index, window=10, max_boxes_to_draw=None, min_score_thresh=.4):
+def update_state(boxes, classes, scores, category_index, window=10, max_boxes_to_draw=None, min_score_thresh=.5):
     """ Revise state based on latest frame of information (object boxes)
 
     Args:
