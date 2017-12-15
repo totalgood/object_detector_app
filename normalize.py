00066ba9945e62630d154bd843348ee30e9cc0c4
Ashwin Kannan <ashwin@Ashwins-MacBook-Pro.local>
2017-12-15 12:02:20 -0800

    modified file. No longer need to use. All under NLP branch and filename is transform.py

--- normalize.py
+++ normalize.py
@@ -75,34 +75,34 @@ def normalize_x_and_y(image,xmin,xmax,ymin,ymax):
 	im_width, im_height = im.size
 	if (xmin > xmax):
 		print ("xmin is greater than xmax")
-		return
+		return ("error")
 
 	if (ymin>ymax):
 		print("ymin is greater than ymax")
-		return
+		return ("error")
 
 	if (xmin > im_width):
 		print("xmin is greater than image width")
-		return
+		return ("error")
 
 	if (xmax > im_width):
 		print("xmax is greater than image width")
-		return
+		return ("error")
 
 	if (ymin > im_height):
 		print ("ymin is greater than image height")
-		return
+		return ("error")
 	if (ymax > im_height):
 		print ("ymax is greater than image height")
-		return
+		return ("error")
 
 	if (xmin < 0):
 		print ("xmin < 0")
-		return
+		return ("error")
 
 	if (ymin < 0):
 		print ("ymin < 0")
-		return
+		return ("error")
 	
 	x = (xmin-(im_width/2))/(im_width/2)
 	y = (ymin-(im_height/2))/(im_height/2)
@@ -111,3 +111,53 @@ def normalize_x_and_y(image,xmin,xmax,ymin,ymax):
 	z = 0.0
 	depth = 0.0
 	return(x,y,width,height,z,depth)
+
+def position(a):
+	""" 
+	this function takes in the image as its argument.
+	the function then calls the normalizing function to get the values of x,y, width and height from
+	normalize function. 
+	Once the values of x,y,width and height are received, the function returns what it believes to be the
+	position of the object.
+	The position of the object has been defined in the follwing manner:
+		if the x position is less than half of the image widht, it is in the left side but to check if it is 
+		centered or not, I check the value of x+width of box. If the value of x+width is greater than 0 and the
+		value of x is less than 0, i say that the object is centered. In the case that x < 0 and x+width < 0, 
+		I say the object is to the left. In any case, since the width cannot be negative, if x >= 0, I say that it
+		is to the right as the position. 
+
+	Also have accounted for error messages. 
+	
+	>>> a = (normalize_x_and_y("/Users/ashwin/Documents/Coding/vish.jpg",0,60,0,100))
+	>>> position(a)
+	center
+	>>> a = (normalize_x_and_y("/Users/ashwin/Documents/Coding/vish.jpg",0,10,0,100))
+	left
+	>>> a = (normalize_x_and_y("/Users/ashwin/Documents/Coding/vish.jpg",50,60,0,100))
+	right
+	>>> a = (normalize_x_and_y("/Users/ashwin/Documents/Coding/vish.jpg",40,60,0,100))
+	center
+	>>> a = (normalize_x_and_y("/Users/ashwin/Documents/Coding/vish.jpg",110,60,0,100))
+	xmin is greater than xmax
+	see above error message
+
+	"""
+
+	if (a != "error"):
+		#print("test passed")
+		x,y,width,height,z,depth = a
+		position = ""
+		if (x <= (0)):
+			if (x+width <= 0):
+				position = "left"
+			else:
+				position = "center"
+		if (x >= (0)):
+			position = "right"
+	
+		print (position)
+	else:
+		print("see above error message")
+
+a = (normalize_x_and_y("/Users/ashwin/Documents/Coding/vish.jpg",110,60,0,100))
+position = position(a)
