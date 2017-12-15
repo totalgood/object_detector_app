a6998da896bb6eb7dd6cbfd14054d1b866d477cd
Alex Rosengarten <alex.rosengarten@aira.io>
2017-12-14 16:22:25 -0800

    (Color Hist v1)
    - Created first iteration of color labeling via histograms.
    Going to test it out now + clean up the code.

--- object_detection/color_labeler/core.py
+++ object_detection/color_labeler/core.py
@@ -53,6 +53,18 @@ class ColorLabeler:
         # mask = cv2.erode(mask, None, iterations=2)
         # return mask
 
+    def _get_bbox_center_img(self, img, box):
+
+        # get space measures
+        ymin, xmin, ymax, xmax = box
+        wd = xmax - xmin
+        ht = ymax - ymin
+
+        # Get center area of the object inside the bounding box
+        obj_center = img[(ymin + ht // 2):(ymax - ht // 2), (xmin + wd // 2):(xmax - wd // 2), :]
+
+        return obj_center
+
     def _build_custer_model(self, img, k: int = 5, mask=None) -> KMeans:
 
         # reshape image to vector of pixels
@@ -73,16 +85,77 @@ class ColorLabeler:
 
         return hist
 
-    def label(self, img):
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        mask = self._create_mask(hsv_img)
-        masked_img = cv2.bitwise_and(hsv_img, hsv_img, mask=mask )
-        vhist = cv2.calcHist([hsv_img[2]], [2], mask, [32], [0, 256])
+    def label(self, img, box=None):
 
-        black_hist = vhist[:2].sum()
-        white_hist = vhist[:-2].sum()
+        # Get center of image (via bounding box)
+        if not box:
+            box = (0, img.shape[0], 0, img.shape[1])
 
-        cv2.bitwise_and()
+        cropped_img = self._get_bbox_center_img(img, box)
+
+        # Convert to Hue, Saturation, Value color space
+        hsv_img = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2HSV)
+
+        # Flatten pixels in image
+        flat_img = hsv_img.reshape((hsv_img.shape[0] * hsv_img.shape[1], 3))
+
+        # Calculate indexed histograms of the Value dimension
+        n_bins_val = 32
+        bw_bin_thresh = 3
+
+        bins_val = np.linspace(0, 255, n_bins_val, endpoint=True, dtype='uint8')
+        hist_val_idx = np.digitize(flat_img[:, 2], bins_val)
+        # hist_val, _ = np.histogram(hist_val_idx)
+
+        is_black_pxl = hist_val_idx <= bw_bin_thresh
+        is_white_pxl = hist_val_idx >= (n_bins_val - bw_bin_thresh)
+
+        black_idx = hist_val_idx[is_black_pxl]
+        white_idx = hist_val_idx[is_white_pxl]
+
+        n_black = len(black_idx)
+        n_white = len(white_idx)
+
+        # Calculate histogram of Hue dimension
+        color_flat_img = flat_img[~np.logical_or(black_idx, white_idx), :]  # exclude black or white pixels
+
+        # Mathematical approach doesn't provide human understandable color ranges. Going to hardcode...
+        # color_centers = np.arange(0, 360, 30)  # produces 12 color centers
+        # color_ranges = (color_centers - 15) % 360  # maps to 360 deg bin ranges
+        # bins_hue = color_ranges // 2  # ranges need to fit in uint8 space [0, 179]
+
+        # [red, orange, yellow, green, cyan, blue, purple, pink]
+        bins_hue = [-8, 7, 22, 82, 97, 127, 142, 171]
+        hist_hue, _ = np.histogram(color_flat_img[:, 0], bins=bins_hue)
+
+        total = n_black + n_white + sum(hist_hue)
+
+        output = {
+            'black': n_black / total,
+            'white': n_white / total,
+            'red': hist_hue[0] / total,
+            'orange': hist_hue[1] / total,
+            'yellow': hist_hue[2] / total,
+            'green': hist_hue[3] / total,
+            'cyan': hist_hue[4] / total,
+            'blue': hist_hue[5] / total,
+            'purple': hist_hue[6] / total,
+            'pink': hist_hue[7] / total
+        }
+
+        return output
+
+
+
+
+
+
+
+
+        # vhist = cv2.calcHist([hsv_img], [2], mask, [32], [0, 256])
+        # black_hist = vhist[:2]
+        # white_hist = vhist[:-2]
+
 
 
 
