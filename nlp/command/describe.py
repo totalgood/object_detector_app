95f6c96ccd1ebd48f0d149fd955dd33b94d8b7c7
Alex Rosengarten <alex.rosengarten@aira.io>
2017-12-18 10:47:56 -0800

    (NLP Functions) - Added template string to `Describe` command + started helper fxn - Updated nlp core `__init__.py` - Broke out functions in `nlp/core.py`.

--- nlp/command/describe.py
+++ nlp/command/describe.py
@@ -4,6 +4,8 @@ from nlp.dispatch import Dispatchable
 
 class Describe(Dispatchable):
 
+    obj_desc_tmpl = '{num_obj} {obj_color} {obj_name} to your {rel_pos}'
+
     def __init__(self, state_q):
         self.state_q = state_q
 
@@ -14,3 +16,8 @@ class Describe(Dispatchable):
             description = describe_state(state)
 
             self.send({'response': description}, subtopic=['say'])
+
+
+def count_obj_by_color(state):
+    new_state = {k: len(v) for k, v in state.items()}
+    return new_state
