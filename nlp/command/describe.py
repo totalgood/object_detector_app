593a6f599d3b1547c7f4fb261e7eda5aa99e7b20
Alex Rosengarten <alex.rosengarten@aira.io>
2017-12-19 09:00:55 -0800

    - Cleaned up the `describe.py` file/command.

--- nlp/command/describe.py
+++ nlp/command/describe.py
@@ -4,8 +4,6 @@ from nlp.dispatch import Dispatchable
 
 class Describe(Dispatchable):
 
-    obj_desc_tmpl = '{num_obj} {obj_color} {obj_name} to your {rel_pos}'
-
     def __init__(self, state_q):
         self.state_q = state_q
 
@@ -15,10 +13,6 @@ class Describe(Dispatchable):
         if state:
             description = describe_scene(state)
 
-            self.send({'response': description}, subtopic=['say'])
+            self.send({'response': description})
 
 
-
-def count_obj_by_color(state):
-    new_state = {k: len(v) for k, v in state.items()}
-    return new_state
