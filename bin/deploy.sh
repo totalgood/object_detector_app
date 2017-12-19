efbb32582b410dc8aef25d50c6a11add8486de26
Alex Rosengarten <alex.rosengarten@aira.io>
2017-12-19 11:42:39 -0800

    (Creating Deploy Scripts)
    - can't activate environments unless scripts are ran with `source`
    - Keeping track of run PIDs
    - Keeping track of dates in log

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,6 @@ git pull origin master
 
 /bin/bash ./bin/update_env.sh
 
+date &>> pids.txt
 /bin/bash ./bin/run.sh &
+$! &>> pids.txt
--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,5 @@
 
 source activate object-detection
 
+date &>> log.txt
 python object_detection_app.py -u=rtsp://52.39.224.108:1935/live/myStream &>> log.txt
