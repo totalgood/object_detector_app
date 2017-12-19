6ff918335d0e6986abe845e6bcf1e7008c56d3b2
Alex Rosengarten <alex.rosengarten@aira.io>
2017-12-19 10:27:32 -0800

    - Fixed color command - added default response (no color detected) - will prefer objects in center to other objects

--- nlp/command/color.py
+++ nlp/command/color.py
@@ -1,9 +1,12 @@
 from nlp.dispatch import Dispatchable
+import object_detection.constants as constants
+from nlp.transform import position
 
 
-class DescribeColor(Dispatchable):
+class DescribeObjectColor(Dispatchable):
 
-    color_tmpl = "The {obj_name} is primarily {color}"
+    color_tmpl = 'The {obj_name} is primarily {color}'
+    color_fail_tmpl = 'I cannot determine the color'
 
     def __init__(self, state_q):
         self.state_q = state_q
@@ -13,19 +16,44 @@ class DescribeColor(Dispatchable):
 
         if state:
 
-            obj_name, object_data = state.popitem()
+            if type(state[0]) is not constants.ObjectSeries:
+                vecs = to_object_series_list(state)
+            else:
+                vecs = state
 
-            if object_data:
-                color_freq = object_data[0]['color']
+            center_vecs = list(filter(lambda v: position(v.obj_bbox) is 'center', vecs))
 
-                max_color = color_freq.idxmax()
+            if len(center_vecs) > 0:
+                obj_vec = center_vecs.pop(0)
+            else:
+                obj_vec = vecs.pop(0)
 
-                print(state)
+            max_color = obj_vec.obj_primary_color
+            obj_name = obj_vec['category']
 
-                payload = {
-                    'response': self.color_tmpl.format(obj_name=obj_name, color=max_color)
-                }
+            print(state)
 
-                self.send(payload, subtopic=['say'])
+            payload = {
+                'response': self.color_tmpl.format(obj_name=obj_name, color=max_color)
+            }
 
+            self.send(payload)
+            return
+
+        payload = {
+            'response': self.color_fail_tmpl
+        }
+
+        self.send(payload)
+
+
+class DescribeSceneColor(Dispatchable):
+    # TODO: implement
+
+    def get_scene_color(self):
+        pass
+
+
+def to_object_series_list(state):
+    return [constants.ObjectSeries(obj, index=constants.OBJECT_VECTOR_KEYS) for obj in state]
 
